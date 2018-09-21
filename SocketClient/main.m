bff18ee9d3f8057a69ce034f9b742e9e03cf39c2
rokity <riccardo.amadio1@gmail.com>
2018-09-21 08:25:46 +0200

    funziona maca il bluetooth

--- SocketClient/main.m
+++ SocketClient/main.m
@@ -30,7 +30,6 @@
 #include <sys/ioctl.h>
 #include <sys/file.h>
 
-
 struct arg_struct {
     char *interface;
     char *host;
@@ -41,6 +40,7 @@ struct arg_thread {
     int socket;
     char* interfaccia;
     struct sockaddr_in _sockaddr;
+    int percentuale;
     
 };
 
@@ -95,111 +95,102 @@ char** str_split(char* a_str, const char a_delim)
 
 char buffer[5];
 
-char *readLineAsNSString(FILE *file)
-{
-    size_t rc = fread(buffer, 5, 1, file);
-    if(rc!=NULL)
-        return &buffer;
-    else
-        return NULL;
-}
-          
-          
+/*char *readLineAsNSString(FILE *file)
+ {
+ size_t rc = fread(buffer, 5, 1, file);
+ if(rc!=NULL)
+ return &buffer;
+ else
+ return NULL;
+ }*/
 
-          
-          
+const char* fileBytes;
+NSUInteger lengthFileBytes;
+
+void initStream()
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"foto" ofType:@"jpg"];
+    NSData * fileData = [NSData dataWithContentsOfFile: path];
+    
+    fileBytes = (const char*)[fileData bytes];
+    lengthFileBytes = [fileData length];
+}
+int count=0;
+unsigned char get()
+{
+    unsigned char c;
+    if(count<(int)lengthFileBytes)
+    {
+        c = fileBytes[count];
+        return c;
+    }
+    return c;
+}
+
+
+
 void *myThreadFun(void *arguments)
 {
-   /* struct arg_struct *args = (struct arg_struct *)arguments;
-    struct sockaddr_in sin;
-    int s = socket(AF_INET, SOCK_STREAM,0);
-    bzero((char *) &sin, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = inet_addr(args->host);
-    sin.sin_port = htons(args->port);
-    struct ifreq ifr;
-    ifr.ifr_addr.sa_family = AF_INET;
-    strncpy(ifr.ifr_name, args->interface, IFNAMSIZ-1);
-    ioctl(s, SIOCGIFADDR, &ifr);
-    struct sockaddr* ipaddr = (struct sockaddr*)&ifr.ifr_addr;
-    int reuseOn = 1;
-    bind(s, ipaddr, ipaddr->sa_len );
-    connect(s,  (struct sockaddr*)&sin,sizeof(sin));
-    char* message = args->message;
-    char** p = str_split(message,'\n');
-    int i;
     
-    for (i = 0; *(p  + i); i++)
-    {
-        //reuseOn = 1;
-        //setsockopt(s, IPPROTO_TCP, TCP_NODELAY, (char *) &reuseOn, sizeof(int));
-        //send(s,*(p + i),strlen(*(p + i))+1,0);
-        printf("carattere=[%s]\n", *(p + i));
-        printf(args->interface);
-        //reuseOn = 0;
-        //setsockopt(s, IPPROTO_TCP, TCP_NODELAY, (char *) &reuseOn, sizeof(int));
-        //free(*(p + i));
-    }
-    
-    close(s);
-    printf("%s\n", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
-    NSLog(@"finish \n");
-    pthread_exit(NULL); */
     struct arg_thread *args = (struct arg_thread *)arguments;
-    char* buffer ;
-    NSString* path = [[NSBundle mainBundle] pathForResource:@"package" ofType:@"json"];
-    FILE *file = fopen([path cStringUsingEncoding: NSUTF8StringEncoding], "r");
     unsigned long int reuseOn = (unsigned long int)TRUE;
-
     //while((buffer = readLineAsNSString(file))!=NULL){
-        //setsockopt(args->socket, IPPROTO_TCP, TCP_NODELAY, &reuseOn, sizeof(reuseOn));
-       // UIImage *image = [UIImage imageNamed:@"bull_no_geometrie.jpg"];
-       // NSUInteger size = strlen(buffer);
-       // unsigned char array[size];
-       // NSData* ddata = [NSData dataWithBytes:(const void *)buffer length:sizeof(unsigned char)*size];
+    //setsockopt(args->socket, IPPROTO_TCP, TCP_NODELAY, &reuseOn, sizeof(reuseOn));
     
-    //NSData *data = UIImageJPEGRepresentation(image,FALSE);
-    NSString *filepath = [[NSBundle mainBundle] pathForResource:@"bull_no_geometrie" ofType:@"jpg"];
-    NSData* data = [NSData dataWithContentsOfFile:filepath];
-    printf("%lu", (unsigned long)data.length);
-    uint8_t * bytePtr = (uint8_t  * )[data bytes];
-     NSInteger totalData = [data length] / sizeof(uint8_t);
-    
-    // Here, For getting individual bytes from fileData, uint8_t is used.
-    // You may choose any other data type per your need, eg. uint16, int32, char, uchar, ... .
-    // Make sure, fileData has atleast number of bytes that a single byte chunk would need. eg. for int32, fileData length must be > 4 bytes. Makes sense ?
-    
-    // Now, if you want to access whole data (fileData) as an array of uint8_t
-   
-    //NSData *data = UIImagePNGRepresentation(image);
-        //NSUInteger len = data.length;
-        //uint8_t *bytes = (uint8_t *)[data bytes];
-        //NSMutableString *result = [NSMutableString stringWithCapacity:len * 3];
-        //[result appendString:@"["];
-        //for (NSUInteger i = 0; i < len; i++) {
-          //  if (i) {
-           //     [result appendString:@","];
-           // }
-           // [result appendFormat:@"%d", bytes[i]];
-        //}
-        //[result appendString:@"]"];
+    //printf("%s", buffer);
+    setsockopt(args->socket, IPPROTO_TCP, TCP_NODELAY, 1, sizeof(int));
+    int percentuale = (args->percentuale * (int)lengthFileBytes)/100;
+    printf("%d", percentuale);
+    for(int i =0;i<percentuale;i=i+1)
+    {
+        unsigned char a[1] ;
+        unsigned int local_count = count;
+        a[0]=get();
+
+        //strncpy(buff, &fileBytes[i], 3);
+        //char str[12];
+        /*
+         sprintf(str, "%d", i);
+         NSError* error = nil;
+         NSString *buffer = [NSString stringWithFormat:@"%s", buff];
+         NSString *strString = [NSString stringWithFormat:@"%s", str];
+         NSArray *array =[NSArray arrayWithObjects: buffer,strString, nil];
+         NSData *jsonData = [NSJSONSerialization dataWithJSONObject:array options:NSJSONWritingPrettyPrinted error:&error];*/
+        //printf(aByte);
+        //char *msg[2] ;
+        //msg[0] = aByte;
+        //msg[1] = "\n";
+        
+        
+        unsigned char c[2];
+        memcpy(c, (unsigned char*)&local_count, 2);
+        printf("%lu", sizeof(c));
         printf("\n");
-        printf("%ld", (long)totalData);
-        printf("\n");
-        printf("%s", bytePtr);
-        send(args->socket,bytePtr, totalData,0);
-       // printf("char=[%s]\n", byteData );
-        printf("\n");
+        printf("chunkid ");
         printf("%s", args->interfaccia);
         printf("\n");
-        //usleep(1000);
-        
+        printf("%s",c);
+        printf("\n");
+        unsigned char d[3];
+        d[0]=a[0];
+        for(int i=0;i<2;i++)
+        {
+            d[i+1]=c[i];
+        }
+        send(args->socket, d, 3, 0);
+        count++;
+    }
+    //printf("sended");
+    //printf("%d", count);
+    //usleep(1000);
+    
     //}
+    printf("%d", count);
     close(args->socket);
     
-        //reuseOn = 0;
-        //setsockopt(s, IPPROTO_TCP, TCP_NODELAY, (char *) &reuseOn, sizeof(int));
-        //free(*(p + i));
+    //reuseOn = 0;
+    //setsockopt(s, IPPROTO_TCP, TCP_NODELAY, (char *) &reuseOn, sizeof(int));
+    //free(*(p + i));
     
     pthread_exit(NULL);
 }
@@ -212,40 +203,41 @@ int main(int argc, char * argv[]) {
     @autoreleasepool {
         
         pthread_t threads[NUM_THREADS];
-        
-        printf("Before Thread\n");
-        int port = 50000;
+        initStream();
+        int port = 3306;
         struct arg_struct *sim_args = (struct arg_struct *)malloc(sizeof(struct arg_struct));
-        sim_args->host= "52.14.182.175";
+        sim_args->host= "18.191.173.32";
         sim_args->interface = "pdp_ip0";
-        sim_args->port = port;
+        sim_args->port = 3306;
         struct arg_struct *wifi_args = (struct arg_struct *)malloc(sizeof(struct arg_struct));
-        wifi_args->host = "192.168.1.107";
+        wifi_args->host = "10.0.3.12";
         wifi_args->interface = "en0";
-        wifi_args->port = port;
-
+        wifi_args->port = 2000;
+        
+        
         /** PREPARO SOCKET SIM **/
-//        struct sockaddr_in sin_sim;
-//        int sim_socket = socket(AF_INET, SOCK_STREAM,0);
-//        bzero((char *) &sin_sim, sizeof(sin_sim));
-//        sin_sim.sin_family = AF_INET;
-//        sin_sim.sin_addr.s_addr = inet_addr(sim_args->host);
-//        sin_sim.sin_port = htons(sim_args->port);
-//        struct ifreq ifr_sim;
-//        ifr_sim.ifr_addr.sa_family = AF_INET;
-//        strncpy(ifr_sim.ifr_name, sim_args->interface, IFNAMSIZ-1);
-//        ioctl(sim_socket, SIOCGIFADDR, &ifr_sim);
-//        struct sockaddr* ipaddr_sim = (struct sockaddr*)&ifr_sim.ifr_addr;
-//        int reuseOn_sim = 1;
-//        bind(sim_socket, ipaddr_sim, ipaddr_sim->sa_len );
-//        connect(sim_socket,  (struct sockaddr*)&sin_sim,sizeof(sin_sim));
-//
-//        struct arg_thread *sim_thread_args = (struct arg_thread *)malloc(sizeof(struct arg_thread));
-//        sim_thread_args->socket = sim_socket;
-//        sim_thread_args->interfaccia = "sim";
-//        sim_thread_args->_sockaddr =sin_sim;
+                struct sockaddr_in sin_sim;
+                int sim_socket = socket(AF_INET, SOCK_STREAM,0);
+                bzero((char *) &sin_sim, sizeof(sin_sim));
+                sin_sim.sin_family = AF_INET;
+                sin_sim.sin_addr.s_addr = inet_addr(sim_args->host);
+                sin_sim.sin_port = htons(sim_args->port);
+                struct ifreq ifr_sim;
+                ifr_sim.ifr_addr.sa_family = AF_INET;
+                strncpy(ifr_sim.ifr_name, sim_args->interface, IFNAMSIZ-1);
+                ioctl(sim_socket, SIOCGIFADDR, &ifr_sim);
+                struct sockaddr* ipaddr_sim = (struct sockaddr*)&ifr_sim.ifr_addr;
+                int reuseOn_sim = 1;
+                bind(sim_socket, ipaddr_sim, ipaddr_sim->sa_len );
+                connect(sim_socket,  (struct sockaddr*)&sin_sim,sizeof(sin_sim));
+        
+                struct arg_thread *sim_thread_args = (struct arg_thread *)malloc(sizeof(struct arg_thread));
+                sim_thread_args->socket = sim_socket;
+                sim_thread_args->interfaccia = "sim";
+                sim_thread_args->_sockaddr =sin_sim;
+                sim_thread_args->percentuale = 50;
         /** FINE PREPARAZIONE SOCKET SIM **/
-
+        
         /** PREPARO SOCKET WIFI**/
         struct sockaddr_in sin_wifi;
         int wifi_socket = socket(AF_INET, SOCK_STREAM,0);
@@ -265,20 +257,28 @@ int main(int argc, char * argv[]) {
         wifi_thread_args->socket = wifi_socket;
         wifi_thread_args->interfaccia = "wifi";
         wifi_thread_args->_sockaddr =sin_wifi;
+        wifi_thread_args->percentuale = 50;
         /** FINE PREPARAZIONE SOCKET WIFI **/
-        printf("lancio threads");
-      
-        //pthread_create(&threads[0], NULL, myThreadFun, (void*)sim_thread_args);
-        pthread_create(&threads[1], NULL, myThreadFun,  (void*)wifi_thread_args);
+        for (int i = 0; i < NUM_THREADS; i++)
+        {
+            if (i == 0)
+                pthread_create(&threads[0], NULL, myThreadFun, (void*)sim_thread_args);
+            else
+                pthread_create(&threads[1], NULL, myThreadFun,  (void*)wifi_thread_args);
+            
+        }
+        for (int i = 0; i < 2; i++)
+            pthread_join(threads[i], NULL);
+        
         //pthread_create(&threads[0], NULL, myThreadFun, (void*)sim_args);
         //pthread_create(&threads[1], NULL, myThreadFun,  (void*)wifi_args);
         //pthread_join(threads[1], NULL);pthread_join(threads[0], NULL);
-        printf("After Thread\n");
         /* block until all threads complete
-        for (int i = 0; i < NUM_THREADS; ++i) {
-            pthread_join(threads[i], NULL);
-        }*/
-//
+         for (int i = 0; i < NUM_THREADS; ++i) {
+         pthread_join(threads[i], NULL);
+         }*/
+        //
+        
         
         
         
@@ -287,6 +287,7 @@ int main(int argc, char * argv[]) {
     
     
 }
+
 
 
 
